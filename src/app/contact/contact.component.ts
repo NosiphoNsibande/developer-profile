22dd226a783212cbceff81b6c979eb8a58bc011d
GundohNosipho <nosiphonsibande54@gmail.com>
2023-05-29 14:51:31 +0200

    fixed images paths

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -9,13 +9,13 @@ import { Component } from '@angular/core';
 
 export class ContactComponent {
   books: Book[] = [
-    { title: 'Sara Jakes', id: 1, thumbImage: '../assets/books/book-sara.webp' },
-    { title: 'Richard', id: 2, thumbImage: '../assets/books/book-succeed.jpg' },
-    { title: 'God', id: 3, thumbImage: '../assets/books/book-god.jpg' },
-    { title: 'Lessons', id: 4, thumbImage: '../assets/books/book-lessons.jpg' },
-    { title: 'Fear', id: 5, thumbImage: '../assets/books/book-fear.jpg' },
-    { title: 'Somizi', id: 6, thumbImage: '../assets/books/somizi-book.jpg' },
-    { title: 'T.D Jakes', id: 7, thumbImage: '../assets/books/book-crushing.jpg' },
+    { title: 'Sara Jakes', id: 1, thumbImage: './assets/books/book-sara.webp' },
+    { title: 'Richard', id: 2, thumbImage: './assets/books/book-succeed.jpg' },
+    { title: 'God', id: 3, thumbImage: './assets/books/book-god.jpg' },
+    { title: 'Lessons', id: 4, thumbImage: './assets/books/book-lessons.jpg' },
+    { title: 'Fear', id: 5, thumbImage: './assets/books/book-fear.jpg' },
+    { title: 'Somizi', id: 6, thumbImage: './assets/books/somizi-book.jpg' },
+    { title: 'T.D Jakes', id: 7, thumbImage: './assets/books/book-crushing.jpg' },
   ];
 }
 
